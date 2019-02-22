18f6e24c1a1f53542862e8c8a6d2f72c9bd4a66c
Cian Higgins <cianhiggins@Cians-MacBook-Pro.local>
2019-02-22 15:10:19 +0000

    c++

---
+++ C++/FSA/cmake-build-debug/CMakeFiles/pics.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pics.dir/tester_pictures.cpp.o"
+  "pics.pdb"
+  "pics"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
